347bd6d0131f3996c97c9174ec3db2f575a1d821
Oscar Castro <oscar.a.castro818@gmail.com>
2022-11-29 00:30:03 -0600

    fixing git issue

--- src/codeSignalExercises.java
+++ src/codeSignalExercises.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class codeSignalExercises {
 
     public static boolean checkPalindrome (String inputString) {
@@ -16,12 +19,34 @@ public class codeSignalExercises {
 //        return inputString.equals(new StringBuilder(inputString).reverse().toString());
     }
 
+    public static int adjacentElementsProduct (int [] inputArray) {
+        ArrayList<Integer> numList = new ArrayList<>();
+        for (int i = 0; i < inputArray.length - 1; i++) {
+            numList.add(inputArray[i] * inputArray[i + 1]);
+        }
+//        int maximum = numList.get(0);
+//        for (int i = 1; i < numList.size(); i++) {
+//            if (maximum < numList.get(i))
+//                maximum = numList.get(i);
+//        }
+        System.out.println(Collections.max(numList));
+//        return maximum;
+        return Collections.max(numList);
+    }
+
 
 
 
     public static void main(String[] args) {
         String word = "oscar";
         checkPalindrome(word);
+
+        int [] testArray = {5, 6, -4, 2, 3, 2, -23};
+        adjacentElementsProduct(testArray);
+
+
     }
+
+
 
 }
