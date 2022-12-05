36f0cfb45df39d3c289409aa4567bd75b765304e
Oscar Castro <oscar.a.castro818@gmail.com>
2022-12-04 23:45:29 -0600

    feat: created methods that pass test cases

--- src/main/java/SmoothSailing.java
+++ src/main/java/SmoothSailing.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class SmoothSailing {
@@ -32,7 +34,55 @@ public class SmoothSailing {
         }
         return maxMatch;
     }
+
+    public boolean isLucky(int n) {
+        int sum = 0;
+        int sum2 = 0;
+        String str = Integer.toString(n);
+        int strLen = str.length();
+
+        for (int i = 0; i < strLen/2; i++) {
+            String s = Character.toString(str.charAt(i));
+            String s2 = Character.toString(str.charAt(i + strLen/2));
+            int num = Integer.parseInt(s);
+            int num2 = Integer.parseInt(s2);
+            sum += num;
+            sum2 += num2;
+        }
+        return sum == sum2;
+    }
+
+    public int[] sortByHeight(int[] a) {
+        List<Integer> aLink = new LinkedList<>();
+        List<Integer> treeIndex = new ArrayList<>();
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == -1) {
+                treeIndex.add(i);
+            } else {
+                aLink.add(a[i]);
+            }
+        }
+        Collections.sort(aLink);
+        for (int j = 0; j < aLink.size(); j++) {
+            for (int k = 0; k < treeIndex.size(); k++) {
+                if (j == treeIndex.get(k)) {
+                    aLink.add(j, -1);
+                }
+            }
+            a[j] = aLink.get(j);
+        }
+        return a;
+    }
+
+
+    public String reverseInParentheses(String inputString) {
+        return inputString;
+    }
+
+
 }
+
+
 
 
 
