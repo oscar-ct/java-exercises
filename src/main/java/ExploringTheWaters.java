594670abec8419ecb5a9863ea4a8543e53853a94
Oscar Castro <oscar.a.castro818@gmail.com>
2022-12-05 00:16:23 -0600

    feat: wrote methods, passed all junit tests

--- src/main/java/ExploringTheWaters.java
+++ src/main/java/ExploringTheWaters.java
@@ -14,4 +14,34 @@ public class ExploringTheWaters {
         }
         return new int[]{sum, sum2};
     }
+
+    public String[] addBorder(String[] picture) {
+        List<String> list = new LinkedList<>();
+        for (int i = 0; i < picture.length; i++) {
+            picture[i] = "*" + picture[i] + "*";
+            list.add(picture[i]);
+        }
+        String s = new String();
+        for (int j = 0; j < picture[0].length(); j++) {
+            s += "*";
+        }
+        list.add(0, s);
+        list.add(picture.length + 1, s);
+        System.out.println(list);
+        return list.toArray(String[]::new);
+    }
+
+    public boolean areSimilar(int[] a, int[] b) {
+        int count = 0;
+        int sum = 1;
+        int sum2 = 1;
+        for (int i = 0; i < b.length; i++) {
+            if (a[i] != b[i]) {
+                count++;
+                sum *= a[i];
+                sum2 *= b[i];
+            }
+        }
+        return count < 3 && sum == sum2;
+    }
 }
