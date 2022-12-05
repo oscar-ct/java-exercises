8ad1479b7477a82c3e7d88783ca7dbd53fe16076
Oscar Castro <oscar.a.castro818@gmail.com>
2022-12-04 23:44:17 -0600

    feat: created junit test cases

--- src/test/java/SmoothSailingTest.java
+++ src/test/java/SmoothSailingTest.java
@@ -22,4 +22,22 @@ public class SmoothSailingTest {
         Assert.assertEquals(26, smoothSailing.commonCharacterCount(s1, s2));
     }
 
+    @Test
+    public void testIsLucky() {
+        int n = 123321;
+        int m = 238017000;
+        Assert.assertTrue(smoothSailing.isLucky(n));
+        Assert.assertFalse(smoothSailing.isLucky(m));
+    }
+
+    @Test
+    public void testSortByHeight() {
+        int[] a = {-1, 150, 190, 170, -1, -1, 160, 180};
+        int[] aSolution = {-1, 150, 160, 170, -1, -1, 180, 190};
+        int[] b = {23, 54, -1, 43, 1, -1, -1, 77, -1, -1, -1, 3};
+        int[] bSolution = {1, 3, -1, 23, 43, -1, -1, 54, -1, -1, -1, 77};
+        Assert.assertArrayEquals(aSolution, smoothSailing.sortByHeight(a));
+        Assert.assertArrayEquals(bSolution, smoothSailing.sortByHeight(b));
+    }
+
 }
