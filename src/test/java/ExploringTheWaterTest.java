7514c4aaec1ed96b8b05d9dbd047f01b47067653
Oscar Castro <oscar.a.castro818@gmail.com>
2022-12-05 00:13:48 -0600

    feat: create additonal junit test casses

--- src/test/java/ExploringTheWaterTest.java
+++ src/test/java/ExploringTheWaterTest.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 
 public class ExploringTheWaterTest {
     final ExploringTheWaters exploringTheWaters = new ExploringTheWaters();
+
     @Test
     public void testAlternatingSums() {
         int[] a = {50, 60, 60, 45, 70};
@@ -11,5 +12,32 @@ public class ExploringTheWaterTest {
         int[] bSolution = {150, 151};
         Assert.assertArrayEquals(aSolution, exploringTheWaters.alternatingSums(a));
         Assert.assertArrayEquals(bSolution, exploringTheWaters.alternatingSums(b));
+    }
+
+    @Test
+    public void testAddBorder() {
+        String[] a = {"abc",
+                "ded"};
+        String[] aSolution = {"*****",
+                "*abc*",
+                "*ded*",
+                "*****"};
+        String[] b = {"wzy**"};
+        String[] bSolution = {"*******",
+                "*wzy***",
+                "*******"};
+        Assert.assertArrayEquals(aSolution, exploringTheWaters.addBorder(a));
+        Assert.assertArrayEquals(bSolution, exploringTheWaters.addBorder(b));
+    }
+
+    @Test
+    public void testAreSimilar() {
+        int[] a = {1, 2, 1, 2, 2, 1};
+        int[] b = {2, 2, 1, 1, 2, 1};
+        int[] x = {832, 998, 148, 570, 533, 561, 894, 147, 455, 279};
+        int[] y = {832, 570, 148, 998, 533, 561, 455, 147, 894, 279};
+        Assert.assertTrue(exploringTheWaters.areSimilar(a, b));
+        Assert.assertFalse(exploringTheWaters.areSimilar(x, y));
+
     }
 }
