e2d9f2ae73cd21682afea2c11e2e37c3cada41f0
Oscar Castro <oscar.a.castro818@gmail.com>
2022-12-10 23:59:54 -0600

    feat: working on solution to isIPv4Address challange, current solution does not pass

--- src/test/java/IslandOfKnowledgeTest.java
+++ src/test/java/IslandOfKnowledgeTest.java
@@ -1,6 +1,10 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class IslandOfKnowledgeTest {
     final IslandOfKnowledge islandOfKnowledge = new IslandOfKnowledge();
     @Test
@@ -16,5 +20,39 @@ public class IslandOfKnowledgeTest {
         Assert.assertEquals(3, islandOfKnowledge.arrayMaximalAdjacentDifference(arr));
         int [] arr2 = {-1, 4 ,10, 3, -2};
         Assert.assertEquals(7, islandOfKnowledge.arrayMaximalAdjacentDifference(arr2));
+    }
+    boolean solution(String inputString) {
+        String [] charArr = inputString.split("");
+        String [] numArr = inputString.split("[.]");
+        List<String> acceptableNums = new ArrayList<>();
+        int dotCount = 0;
+        boolean invalidNumCheck = true;
+
+        for (int i = 0; i < charArr.length; i++) {
+            System.out.println(charArr[i]);
+            if (charArr[i].matches("[.]")) {
+                dotCount++;
+            }
+        }
+        for (int j = 0; j < numArr.length; j++) {
+            int n = Integer.parseInt(numArr[j]);
+            if (numArr[j] != "" && n <= 255 && n >= 0) {
+                acceptableNums.add(numArr[j]);
+            }
+        }
+        for (int k = 0; k < acceptableNums.size(); k++) {
+            String [] arr = acceptableNums.get(k).split("");
+            for (int l = 0; l < arr.length; l++) {
+                if (Integer.parseInt(arr[l]) == 0 && arr.length > 1) {
+                    invalidNumCheck = false;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(charArr));
+        System.out.println(Arrays.toString(numArr));
+        System.out.println(dotCount);
+        System.out.println(acceptableNums);
+        return dotCount == acceptableNums.size() - 1 && acceptableNums.size() == 4 && invalidNumCheck;
+
     }
 }
