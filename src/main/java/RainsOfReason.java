4534939c499cfd16ac49d43482063f8d6ad08817
Oscar Castro <oscar.a.castro818@gmail.com>
2022-12-13 23:19:30 -0600

    feat: created method checking for even digits, passed test cases

--- src/main/java/RainsOfReason.java
+++ src/main/java/RainsOfReason.java
@@ -8,4 +8,16 @@ public class RainsOfReason {
         }
         return inputArray;
     }
+    boolean evenDigitsOnly(int n) {
+        String s = "" + n;
+        int evenCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            String ch = Character.toString(s.charAt(i));
+            int num = Integer.parseInt(ch);
+            if (num % 2 != 0) {
+                evenCount++;
+            }
+        }
+        return evenCount == 0;
+    }
 }
