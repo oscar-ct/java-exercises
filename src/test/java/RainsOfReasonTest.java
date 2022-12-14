8ce7344557c156735e6a34f90e44fa8ef8c0c422
Oscar Castro <oscar.a.castro818@gmail.com>
2022-12-13 23:14:56 -0600

    correction: renamed file

--- src/test/java/RainsOfReasonTest.java
+++ src/test/java/RainsOfReasonTest.java
@@ -1,15 +1,15 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-public class ThroughTheFogTest {
-    ThroughTheFog throughTheFog = new ThroughTheFog();
+public class RainsOfReasonTest {
+    RainsOfReason rainsOfReason = new RainsOfReason();
     @Test
     public void arrayReplaceTest() {
         int[] a = {1, 2, 1, 2, 1};
         int[] aSolution =  {1, 2, 1, 2, 1};
-        Assert.assertArrayEquals(aSolution, throughTheFog.arrayReplace(a, 2, 2));
+        Assert.assertArrayEquals(aSolution, rainsOfReason.arrayReplace(a, 2, 2));
         int[] b = {1, 2, 3, 4, 5};
         int[] bSolution = {1, 2, 0, 4, 5};
-        Assert.assertArrayEquals(bSolution, throughTheFog.arrayReplace(b, 3, 0));
+        Assert.assertArrayEquals(bSolution, rainsOfReason.arrayReplace(b, 3, 0));
     }
 }
