8dde1e35cd386e5e6f32a5adcdfe892ff9410b69
Oscar Castro <oscar.a.castro818@gmail.com>
2022-12-18 00:05:55 -0600

    feat: created new class for frequently used methods, added isNumeric

---
+++ src/main/java/Utils.java
@@ -0,0 +1,10 @@
+public class Utils {
+    static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
