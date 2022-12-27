1d2d0d822746afadb827269258ce5ca460e7308e
Oscar Castro <oscar.a.castro818@gmail.com>
2022-12-26 22:43:36 -0600

    feat: creathed method that passed all stringRearrangement test cases

--- src/main/java/RainsOfReason.java
+++ src/main/java/RainsOfReason.java
@@ -78,6 +78,31 @@ public class RainsOfReason {
         return count;
     }
 
+    int absoluteValuesSumMinimization(int[] a) {
+        int[] arr = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            int total = 0;
+            for (int j = 0; j < a.length; j++) {
+                total += Math.abs(a[i] - a[j]);
+            }
+            arr[i] = total;
+        }
+        int min = arr[0];
+        for (int k = 1; k < arr.length; k++) {
+            if (arr[k] < min) {
+                min = arr[k];
+            }
+        }
+        int index = 0;
+        for (int l = 0; l < arr.length; l++) {
+            if (arr[l] == min) {
+                index = l;
+                break;
+            }
+        }
+        return a[index];
+    }
+
 
 
 }
