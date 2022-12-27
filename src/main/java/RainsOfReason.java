5d2f9459a8dce79dbc9a2d13bd188440d957e2f5
Oscar Castro <oscar.a.castro818@gmail.com>
2022-12-26 23:03:20 -0600

    feat: moved test cases and methods over to throughTheFog class

--- src/main/java/RainsOfReason.java
+++ src/main/java/RainsOfReason.java
@@ -63,46 +63,6 @@ public class RainsOfReason {
         return light.contains(cell1) && light.contains(cell2) || dark.contains(cell1) && dark.contains(cell2);
     }
 
-    int circleOfNumbers(int n, int firstNumber) {
-        return (n/2 + firstNumber) % n;
-    }
-
-    int depositProfit(int deposit, int rate, int threshold) {
-        int total = deposit;
-        int count = 0;
-        while (total < threshold) {
-            double amount = (double)rate / 100 * total;
-            total += amount;
-            count++;
-        }
-        return count;
-    }
-
-    int absoluteValuesSumMinimization(int[] a) {
-        int[] arr = new int[a.length];
-        for (int i = 0; i < a.length; i++) {
-            int total = 0;
-            for (int j = 0; j < a.length; j++) {
-                total += Math.abs(a[i] - a[j]);
-            }
-            arr[i] = total;
-        }
-        int min = arr[0];
-        for (int k = 1; k < arr.length; k++) {
-            if (arr[k] < min) {
-                min = arr[k];
-            }
-        }
-        int index = 0;
-        for (int l = 0; l < arr.length; l++) {
-            if (arr[l] == min) {
-                index = l;
-                break;
-            }
-        }
-        return a[index];
-    }
-
 
 
 }
