4024906375954968c910414fffdd2327862812c3
Oscar Castro <oscar.a.castro818@gmail.com>
2022-12-28 00:19:55 -0600

    created new method, solved extractEachKth challange

--- src/main/java/ThroughTheFog.java
+++ src/main/java/ThroughTheFog.java
@@ -38,4 +38,17 @@ public class ThroughTheFog {
         }
         return a[index];
     }
+    int[] extractEachKth(int[] inputArray, int k) {
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < inputArray.length; i++) {
+            if ((i+1) % k != 0) {
+                arr.add(inputArray[i]);
+            }
+        }
+        int[] a = new int[arr.size()];
+        for (int j = 0; j < arr.size(); j++) {
+            a[j] = arr.get(j);
+        }
+        return a;
+    }
 }
