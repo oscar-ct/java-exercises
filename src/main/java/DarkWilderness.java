1e7fa039e22a7bbd0310542670d09234cc86699e
Oscar Castro <oscar.a.castro818@gmail.com>
2023-01-03 04:06:51 -0600

    feat: created new method that passes all knapsackLight challange test cases

--- src/main/java/DarkWilderness.java
+++ src/main/java/DarkWilderness.java
@@ -11,4 +11,28 @@ public class DarkWilderness {
         }
         return b;
     }
+    public int knapsackLight(int value1, int weight1, int value2, int weight2, int maxW) {
+        boolean bothValuesUnderMaxW = false, singleValueUnderMaxW = false, sumValuesUnderMaxW = false;
+        if (weight1 + weight2 <= maxW) {
+            sumValuesUnderMaxW = true;
+        } else if (weight1 <= maxW && weight2 <= maxW) {
+            bothValuesUnderMaxW = true;
+        } else if (weight1 <= maxW || weight2 <= maxW) {
+            singleValueUnderMaxW = true;
+        }
+        if (singleValueUnderMaxW) {
+            if (weight1 <= maxW) {
+                return value1;
+            } else {
+                return value2;
+            }
+        }
+        if (bothValuesUnderMaxW) {
+            return Math.max(value1, value2);
+        }
+        if (sumValuesUnderMaxW) {
+            return value1 + value2;
+        }
+        return 0;
+    }
 }
