4f1349dc4850a4490cbe1b15a5348303fd419d32
Oscar Castro <oscar.a.castro818@gmail.com>
2023-01-06 23:20:55 -0600

    feat: created test cases for findEmailDomain challange

--- src/test/java/EruptionOfLightTest.java
+++ src/test/java/EruptionOfLightTest.java
@@ -1,2 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class EruptionOfLightTest {
+    EruptionOfLight eruptionOfLight = new EruptionOfLight();
+    @Test
+    public void findEmailDomainTest() {
+        Assert.assertEquals("example.com", eruptionOfLight.findEmailDomain("prettyandsimple@example.com"));
+        Assert.assertEquals("usual.com", eruptionOfLight.findEmailDomain("\"very.unusual.@.unusual.com\"@usual.com"));
+        Assert.assertEquals("yahoo.com", eruptionOfLight.findEmailDomain("\"much.more unusual\"@yahoo.com"));
+    }
 }
