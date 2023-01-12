0bfc9549296414803de5f99070c8d31bb44e2754
Oscar Castro <oscar.a.castro818@gmail.com>
2023-01-11 23:44:21 -0600

    feat: created test cases for electionWinners challange

--- src/test/java/EruptionOfLightTest.java
+++ src/test/java/EruptionOfLightTest.java
@@ -22,5 +22,11 @@ public class EruptionOfLightTest {
         Assert.assertFalse(eruptionOfLight.isMAC48Address("not a MAC-48 address"));
         Assert.assertFalse(eruptionOfLight.isMAC48Address("G0-00-00-00-00-00"));
     }
+    @Test
+    public void electionWinnersTest() {
+        Assert.assertEquals(2, eruptionOfLight.electionWinners(new int[]{3, 1, 1, 3, 1}, 2));
+        Assert.assertEquals(2, eruptionOfLight.electionWinners(new int[]{2, 3, 5, 2}, 3));
+        Assert.assertEquals(1, eruptionOfLight.electionWinners(new int[]{5, 1, 3, 4, 1}, 0));
+    }
 
 }
