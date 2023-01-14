d6192c7cd5f2e505cee6ee14b280bf19d95783f6
Oscar Castro <oscar.a.castro818@gmail.com>
2023-01-13 23:05:51 -0600

    feat: created method that passes all lineEncoding test cases

--- src/main/java/RainbowOfClarity.java
+++ src/main/java/RainbowOfClarity.java
@@ -2,4 +2,23 @@ public class RainbowOfClarity {
     boolean isDigit(char symbol) {
         return Character.isDigit(symbol);
     }
+    String lineEncoding(String s) {
+        s += '!';
+        String ns = "";
+        int count = 1;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i+1)) {
+                count++;
+            } else {
+                if (count > 1) {
+                    ns += Integer.toString(count) + s.charAt(i);
+                } else {
+                    ns += s.charAt(i);
+                }
+                count = 1;
+            }
+        }
+        return ns;
+    }
+
 }
