8bf4d671ea7f5d8f0892539f50383a51637e34d5
Oscar Castro <oscar.a.castro818@gmail.com>
2023-01-15 23:18:57 -0600

    feat: created method that passes test cases however this solution fail 2 hidden test cases on codesignal, attempting to identify these

--- src/main/java/RainbowOfClarity.java
+++ src/main/java/RainbowOfClarity.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class RainbowOfClarity {
     boolean isDigit(char symbol) {
         return Character.isDigit(symbol);
@@ -20,5 +23,28 @@ public class RainbowOfClarity {
         }
         return ns;
     }
-
+    String longestWord(String text) {
+        ArrayList<String> arrList = new ArrayList<>();
+        String[] arr = text.split(" ");
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            String s = arr[i];
+            String n = "";
+            for (int y = 0; y < arr[i].length(); y++) {
+                if (Character.isAlphabetic(s.charAt(y))) {
+                    n += s.charAt(y);
+                }
+            }
+            arrList.add(n);
+        }
+        String longestWord = "";
+        int max = 0;
+        for (int j = 0; j < arrList.size(); j++) {
+            if (arrList.get(j).length() > max) {
+                max = arrList.get(j).length();
+                longestWord = arrList.get(j);
+            }
+        }
+        return longestWord;
+    }
 }
