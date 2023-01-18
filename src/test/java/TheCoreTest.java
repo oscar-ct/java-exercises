1ff7886d57f34303e22869191236b566fd6d41c9
Oscar Castro <oscar.a.castro818@gmail.com>
2023-01-17 23:05:02 -0600

    feat: created test cases for candies challange

--- src/test/java/TheCoreTest.java
+++ src/test/java/TheCoreTest.java
@@ -15,4 +15,13 @@ public class TheCoreTest {
         Assert.assertEquals(99999999, theCore.largestNumber(8));
         Assert.assertEquals(9, theCore.largestNumber(1));
     }
+    @Test
+    public void candiesTest() {
+        Assert.assertEquals(9, theCore.candies(3, 10));
+        Assert.assertEquals(2, theCore.candies(1, 2));
+        Assert.assertEquals(0, theCore.candies(10, 5));
+        Assert.assertEquals(4, theCore.candies(4, 4));
+        Assert.assertEquals(12, theCore.candies(4, 15));
+        Assert.assertEquals(99, theCore.candies(9, 100));
+    }
 }
