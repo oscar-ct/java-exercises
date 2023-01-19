66cce0cc7555c93f4594e1d34d27f69bbb02f00a
Oscar Castro <oscar.a.castro818@gmail.com>
2023-01-18 23:28:36 -0600

    feat: found solution to reverseInParentheses, failed to create my pwn method

--- src/main/java/TheCore.java
+++ src/main/java/TheCore.java
@@ -27,5 +27,14 @@ public class TheCore {
 //    int candies(int n, int m) {
 //        return m - m%n;
 //    }
-
+    String reverseInParentheses(String inputString) {
+        StringBuilder str = new StringBuilder(inputString);
+        int start, end;
+        while(str.indexOf("(") != -1){
+            start = str.lastIndexOf("(");
+            end = str.indexOf(")", start);
+            str.replace(start, end + 1, new StringBuilder(str.substring(start+1, end)).reverse().toString());
+        }
+        return str.toString();
+    }
 }
