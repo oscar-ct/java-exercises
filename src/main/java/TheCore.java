a4faccf0e638cdbb7ccae77a7405ecc4bf4a3894
Oscar Castro <oscar.a.castro818@gmail.com>
2023-01-25 23:19:51 -0600

    feat: created solution to phoneCall test cases, also commented other users solution for notes

--- src/main/java/TheCore.java
+++ src/main/java/TheCore.java
@@ -46,4 +46,31 @@ public class TheCore {
 //        int minutes = n%60;
 //        return hours/10 + hours%10 + minutes/10 + minutes%10;
 //    }
+    int phoneCall(int min1, int min2_10, int min11, int s) {
+        int sum = min1, min2count = 0, min11count = 0;
+        if (min1 == s) {
+            return 1;
+        } else if (s < min1) {
+            return 0;
+        }
+        while (s >= sum && min2count < 9) {
+            sum += min2_10;
+            if (s >= sum) {
+                min2count++;
+            }
+        }
+        if (sum < s) {
+            for (int i = sum; i <= s; i += min11) {
+                min11count++;
+            }
+            min11count--;
+        }
+        return min2count + min11count + 1;
+    }
+//    int phoneCall(int min1, int min2_10, int min11, int s) {
+//        int m;
+//        for (m = 1; s >= 0; m++) s -= m == 1 ? min1 : m <= 10 ? min2_10 : min11;
+//        return m - 2;
+//    }
+
 }
