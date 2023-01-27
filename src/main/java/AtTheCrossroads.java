a5bf473fe027dc951f5a4cd9146460c1a7804328
Oscar Castro <oscar.a.castro818@gmail.com>
2023-01-26 23:31:57 -0600

    feat: created test cases and solution methods for reachNextLevel challange

--- src/main/java/AtTheCrossroads.java
+++ src/main/java/AtTheCrossroads.java
@@ -1,2 +1,6 @@
 public class AtTheCrossroads {
+    boolean reachNextLevel(int experience, int threshold, int reward) {
+        return threshold <= experience+reward;
+    }
+
 }
--- src/test/java/AtTheCrossroadsTest.java
+++ src/test/java/AtTheCrossroadsTest.java
@@ -1,2 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class AtTheCrossroadsTest {
+    AtTheCrossroads atTheCrossroads = new AtTheCrossroads();
+    @Test
+    public void reachNextLevelTest() {
+        Assert.assertTrue(atTheCrossroads.reachNextLevel(10, 15, 5));
+        Assert.assertFalse(atTheCrossroads.reachNextLevel(10, 15,4));
+        Assert.assertTrue(atTheCrossroads.reachNextLevel(3, 6 ,4));
+    }
 }
