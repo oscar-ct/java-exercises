64574233f07f339d89ead8a6498bf6877179fa09
Oscar Castro <oscar.a.castro818@gmail.com>
2023-01-27 23:41:50 -0600

    feat: wrote method solution to extraNumber challange, passed all test cases

--- src/main/java/AtTheCrossroads.java
+++ src/main/java/AtTheCrossroads.java
@@ -2,5 +2,13 @@ public class AtTheCrossroads {
     boolean reachNextLevel(int experience, int threshold, int reward) {
         return threshold <= experience+reward;
     }
-
+    int extraNumber(int a, int b, int c) {
+        if (a == b) {
+            return c;
+        }
+        if (a == c) {
+            return b;
+        }
+        return a;
+    }
 }
