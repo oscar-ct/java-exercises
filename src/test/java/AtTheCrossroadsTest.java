8e740656fadf69659a1103a69c2e7d1ff748e6e5
Oscar Castro <oscar.a.castro818@gmail.com>
2023-01-27 23:40:58 -0600

    feat: wrote test cases for extraNumber challange

--- src/test/java/AtTheCrossroadsTest.java
+++ src/test/java/AtTheCrossroadsTest.java
@@ -9,4 +9,12 @@ public class AtTheCrossroadsTest {
         Assert.assertFalse(atTheCrossroads.reachNextLevel(10, 15,4));
         Assert.assertTrue(atTheCrossroads.reachNextLevel(3, 6 ,4));
     }
+    @Test
+    public void extraNumberTest() {
+        Assert.assertEquals(7, atTheCrossroads.extraNumber(2, 7, 2));
+        Assert.assertEquals(3, atTheCrossroads.extraNumber(3, 2, 2));
+        Assert.assertEquals(1, atTheCrossroads.extraNumber(5, 5, 1));
+        Assert.assertEquals(999, atTheCrossroads.extraNumber(99, 999, 99));
+        Assert.assertEquals(3, atTheCrossroads.extraNumber(500000000, 3, 500000000));
+    }
 }
