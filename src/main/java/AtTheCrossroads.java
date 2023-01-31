9806c6f53f34c8d32b805f89d899c91dd6401014
Oscar Castro <oscar.a.castro818@gmail.com>
2023-01-30 23:59:19 -0600

    feat: couldnt solve challange, leaving method for future reference

--- src/main/java/AtTheCrossroads.java
+++ src/main/java/AtTheCrossroads.java
@@ -14,4 +14,14 @@ public class AtTheCrossroads {
 //    int extraNumber (int a, int b, int c) {
 //        return a^b^c;
 //    }
+
+    //  Given integers a and b, determine whether the following pseudocode results in an infinite loop
+    //  while a is not equal to b do
+    //  increase a by 1
+    //  decrease b by 1
+    boolean isInfiniteProcess(int a, int b) {
+        return !(b>=a && (b-a)%2==0);
+    }
+    //  this is a solution to a challenge I could not figure out.  I had to google the solution to this. Credit to user mssk.
+    // leaving this here for future reference
 }
